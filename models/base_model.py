7a76c3775a1ef8e4f77bf514c1645aa028b8548f
kaleabendrias <kaleabendrias1212@gmail.com>
2023-08-07 01:58:44 -0700

    finished number 4

--- models/base_model.py
+++ models/base_model.py
@@ -15,11 +15,19 @@ class BaseModel:
         to_dict(self): returns a dictionary containing all keys/values of dict
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ init method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key,value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self,key,value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """print: [<class name>] (<self.id>) <self.__dict__>"""
