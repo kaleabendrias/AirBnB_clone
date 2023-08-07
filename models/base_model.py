50b19edf8f15859e3b119206a915491867b29c13
kaleabendrias <kaleabendrias1212@gmail.com>
2023-08-07 01:37:30 -0700

    fix

---
+++ models/base_model.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+base class
+"""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """class for ewntire proj
+    methods:
+        __str__: prints class name, id and creates dict
+        save(self): update updated_at with the current datetime
+        to_dict(self): returns a dictionary containing all keys/values of dict
+    """
+
+    def __init__(self):
+        """ init method"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """print: [<class name>] (<self.id>) <self.__dict__>"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """updates the public instance attribute updated_at with current"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__"""
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
