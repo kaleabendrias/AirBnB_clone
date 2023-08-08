81c549668ca52e5bd4c5a2d1e74b05f1959623b6
kaleabendrias <kaleabendrias1212@gmail.com>
2023-08-08 05:19:00 -0700

    fix

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""console acting console"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """class HBNBCommand"""
+    prompt = "(hbnb) "
+    dictt = ("BaseModel": BaseModel}
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program using Ctrl+D (EOF)"""
+        print()
+        return True
+
+    def emptyline(self):
+        """Do nothing on empty line"""
+        pass
