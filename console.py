630f28afaa67504bc7f28db3ca915c80b4b7652b
kaleabendrias <kaleabendrias1212@gmail.com>
2023-08-08 05:36:17 -0700

    implemented create

--- console.py
+++ console.py
@@ -23,6 +23,19 @@ class HBNBCommand(cmd.Cmd):
         """Do nothing on empty line"""
         pass
 
+    def do_create(self, arg):
+        """ Creates a new instance of BaseModel"""
+        args = arg.split()
+        if not len(args):
+            print("** class name missing **")
+            return
+        if args[0] in self.dictt.keys():
+            new_inst = self.dictt[args[0]]()
+            new_inst.save()
+            print(new_inst.id)
+        else:
+            print("** class doesn't exist **")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
