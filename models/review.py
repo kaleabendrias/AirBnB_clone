ebecb458b160744e8c9c9b0c80fd747fb6c75e5c
kaleabendrias <kaleabendrias1212@gmail.com>
2023-08-08 22:46:20 -0700

    added review

---
+++ models/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""contains class review"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """class Revew"""
+    place_id = ""
+    user_id = ""
+    text = ""
